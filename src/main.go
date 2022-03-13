6eb4088afb3bf1f44d9fdc20c7fb1738fd9699d9
Dan Craig <danielxcraig@gmail.com>
2022-03-13 20:29:28 +0000

    commit7

--- src/main.go
+++ src/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/danxcraig/demob-main/controllers"
+	"github.com/danxcraig/demob-main/middlewares"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
 
 	server.GET("/", controllers.Get)
 
